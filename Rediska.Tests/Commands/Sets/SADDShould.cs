643c746d60d024d725434970ed9ae258db6fb73a
Pepelev <pepelev@skbkontur.ru>
2019-07-27 13:30:32 +0500

    makes test to run with different redis-servers

--- Rediska.Tests/Commands/Sets/SADDShould.cs
+++ Rediska.Tests/Commands/Sets/SADDShould.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -14,23 +17,53 @@ using Array = System.Array;
 
 namespace Rediska.Tests.Commands.Sets
 {
+    public sealed class RedisCollection : IEnumerable<TestFixtureData>
+    {
+        public IEnumerator<TestFixtureData> GetEnumerator()
+        {
+            var ubuntu = new IPAddress(
+                new byte[] {192, 168, 56, 1}
+            );
+            return new[]
+                {
+                    new TestFixtureData(new IPEndPoint(ubuntu, 50260)).SetArgDisplayNames("redis-2.6"),
+                    new TestFixtureData(new IPEndPoint(ubuntu, 50280)).SetArgDisplayNames("redis-2.8"),
+                    new TestFixtureData(new IPEndPoint(ubuntu, 50320)).SetArgDisplayNames("redis-3.2"),
+                    new TestFixtureData(new IPEndPoint(ubuntu, 50400)).SetArgDisplayNames("redis-4.0"),
+                    new TestFixtureData(new IPEndPoint(ubuntu, 50500)).SetArgDisplayNames("redis-5.0")
+                }
+                .AsEnumerable()
+                .GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
     [Category("RealRedis")]
+    [TestFixtureSource(typeof(RedisCollection))]
     public sealed class SADDShould
     {
+        private readonly IPEndPoint endpoint;
         private LoggingConnection connection;
         private Key key;
+        private TcpClient tcp;
+
+        public SADDShould(IPEndPoint endpoint)
+        {
+            this.endpoint = endpoint;
+        }
 
         [SetUp]
         public async Task SetUpAsync()
         {
-            var tcp = new TcpClient
+            tcp = new TcpClient
             {
                 NoDelay = true
             };
-            var ip = new IPAddress(
-                new byte[] {192, 168, 56, 1}
-            );
-            await tcp.ConnectAsync(ip, 27_000).ConfigureAwait(false);
+            await tcp.ConnectAsync(endpoint.Address, endpoint.Port).ConfigureAwait(false);
             connection = new LoggingConnection(
                 new SimpleConnection(tcp.GetStream())
             );
@@ -63,7 +96,13 @@ namespace Rediska.Tests.Commands.Sets
 
             if (status == TestStatus.Failed)
             {
+                tcp?.Dispose();
+
+                if (key == null || connection == null)
+                    return;
+
                 Console.WriteLine($"Key: {key}");
+
                 var index = 1;
                 foreach (var entry in connection.Log)
                 {
--- Rediska.Tests/Properties/AssemblyInfo.cs
+++ Rediska.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Rediska.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SKB Kontur")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rediska.Tests")]
-[assembly: AssemblyCopyright("Copyright © SKB Kontur 2018")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
