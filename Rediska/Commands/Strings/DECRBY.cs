3e4cef872c8b2173c4012afbb31c1657a8c69ca7
pepelev <pepelev@skbkontur.ru>
2020-07-01 01:07:00 +0500

    implements INCRBY, DECRBY

---
+++ Rediska/Commands/Strings/DECRBY.cs
@@ -0,0 +1,40 @@
+﻿namespace Rediska.Commands.Strings
+{
+    using Protocol;
+    using Protocol.Visitors;
+
+    public sealed class DECRBY : Command<DECRBY.Response>
+    {
+        private static readonly PlainBulkString name = new PlainBulkString("DECRBY");
+        private readonly Key key;
+        private readonly long decrement;
+
+        public DECRBY(Key key, long decrement)
+        {
+            this.key = key;
+            this.decrement = decrement;
+        }
+
+        public override DataType Request => new PlainArray(
+            name,
+            key.ToBulkString(),
+            decrement.ToBulkString()
+        );
+
+        public override Visitor<Response> ResponseStructure => IntegerExpectation.Singleton
+            .Then(valueAfterDecrement => new Response(decrement, valueAfterDecrement));
+
+        public readonly struct Response
+        {
+            public Response(long decrement, long valueAfterDecrement)
+            {
+                Decrement = decrement;
+                ValueAfterDecrement = valueAfterDecrement;
+            }
+
+            public long Decrement { get; }
+            public long ValueBeforeDecrement => ValueAfterDecrement - Decrement;
+            public long ValueAfterDecrement { get; }
+        }
+    }
+}
---
+++ Rediska/Commands/Strings/INCRBY.cs
@@ -0,0 +1,40 @@
+﻿namespace Rediska.Commands.Strings
+{
+    using Protocol;
+    using Protocol.Visitors;
+
+    public sealed class INCRBY : Command<INCRBY.Response>
+    {
+        private static readonly PlainBulkString name = new PlainBulkString("INCRBY");
+        private readonly Key key;
+        private readonly long increment;
+
+        public INCRBY(Key key, long increment)
+        {
+            this.key = key;
+            this.increment = increment;
+        }
+
+        public override DataType Request => new PlainArray(
+            name,
+            key.ToBulkString(),
+            increment.ToBulkString()
+        );
+
+        public override Visitor<Response> ResponseStructure => IntegerExpectation.Singleton
+            .Then(valueAfterIncrement => new Response(increment, valueAfterIncrement));
+
+        public readonly struct Response
+        {
+            public Response(long increment, long valueAfterIncrement)
+            {
+                Increment = increment;
+                ValueAfterIncrement = valueAfterIncrement;
+            }
+
+            public long Increment { get; }
+            public long ValueBeforeIncrement => ValueAfterIncrement - Increment;
+            public long ValueAfterIncrement { get; }
+        }
+    }
+}
