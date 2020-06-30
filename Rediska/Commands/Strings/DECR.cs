c9e3305dc4b007448d25be2cf0c2a641b4b7987c
pepelev <pepelev@skbkontur.ru>
2020-07-01 00:59:45 +0500

    implements INCR and DECR

---
+++ Rediska/Commands/Strings/DECR.cs
@@ -0,0 +1,38 @@
+﻿namespace Rediska.Commands.Strings
+{
+    using Protocol;
+    using Protocol.Visitors;
+
+    public sealed class DECR : Command<DECR.Response>
+    {
+        private static readonly PlainBulkString name = new PlainBulkString("DECR");
+
+        private static readonly Visitor<Response> responseStructure = IntegerExpectation.Singleton
+            .Then(valueAfterDecrement => new Response(valueAfterDecrement));
+
+        private readonly Key key;
+
+        public DECR(Key key)
+        {
+            this.key = key;
+        }
+
+        public override DataType Request => new PlainArray(
+            name,
+            key.ToBulkString()
+        );
+
+        public override Visitor<Response> ResponseStructure => responseStructure;
+
+        public readonly struct Response
+        {
+            public Response(long valueAfterDecrement)
+            {
+                ValueAfterDecrement = valueAfterDecrement;
+            }
+
+            public long ValueBeforeDecrement => ValueAfterDecrement - 1;
+            public long ValueAfterDecrement { get; }
+        }
+    }
+}
---
+++ Rediska/Commands/Strings/INCR.cs
@@ -0,0 +1,38 @@
+﻿namespace Rediska.Commands.Strings
+{
+    using Protocol;
+    using Protocol.Visitors;
+
+    public sealed class INCR : Command<INCR.Response>
+    {
+        private static readonly PlainBulkString name = new PlainBulkString("INCR");
+
+        private static readonly Visitor<Response> responseStructure = IntegerExpectation.Singleton
+            .Then(valueAfterIncrement => new Response(valueAfterIncrement));
+
+        private readonly Key key;
+
+        public INCR(Key key)
+        {
+            this.key = key;
+        }
+
+        public override DataType Request => new PlainArray(
+            name,
+            key.ToBulkString()
+        );
+
+        public override Visitor<Response> ResponseStructure => responseStructure;
+
+        public readonly struct Response
+        {
+            public Response(long valueAfterIncrement)
+            {
+                ValueAfterIncrement = valueAfterIncrement;
+            }
+
+            public long ValueBeforeIncrement => ValueAfterIncrement - 1;
+            public long ValueAfterIncrement { get; }
+        }
+    }
+}
