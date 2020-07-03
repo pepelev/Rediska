a73b1091c664467eceeab3afe402a34baf723f55
pepelev <kran440@gmail.com>
2020-07-04 00:51:24 +0500

    cleanup

--- Rediska/Commands/Server/COMMAND.cs
+++ Rediska/Commands/Server/COMMAND.cs
@@ -3,7 +3,6 @@
     using System.Collections.Generic;
     using Protocol;
     using Protocol.Visitors;
-    using Utils;
 
     public sealed partial class COMMAND : Command<IReadOnlyList<CommandDescription>>
     {
@@ -18,30 +17,5 @@
         public static COMMAND Singleton { get; } = new COMMAND();
         public override DataType Request => request;
         public override Visitor<IReadOnlyList<CommandDescription>> ResponseStructure => responseStructure;
-
-        public sealed class GETKEYS : Command<IReadOnlyList<Key>>
-        {
-            private static readonly PlainBulkString subName = new PlainBulkString("GETKEYS");
-            private readonly IReadOnlyList<BulkString> command;
-
-            public GETKEYS(params BulkString[] command)
-                : this(command as IReadOnlyList<BulkString>)
-            {
-            }
-
-            public GETKEYS(IReadOnlyList<BulkString> command)
-            {
-                this.command = command;
-            }
-
-            public override DataType Request => new PlainArray(
-                new ConcatList<DataType>(
-                    new[] {name, subName},
-                    command
-                )
-            );
-
-            public override Visitor<IReadOnlyList<Key>> ResponseStructure => CompositeVisitors.KeyList;
-        }
     }
 }
