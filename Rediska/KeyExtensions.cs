7c03f593b09df06bebe039963cd33a38bc89170f
pepelev <kran440@gmail.com>
2020-07-04 23:35:49 +0500

    yet another todo

--- Rediska/KeyExtensions.cs
+++ Rediska/KeyExtensions.cs
@@ -4,6 +4,7 @@ namespace Rediska
 {
     public static class KeyExtensions
     {
+        // todo move to Key for performance reasons
         public static BulkString ToBulkString(this Key key) => new PlainBulkString(key.ToBytes());
     }
 }
