0f853a6b93c14aa7b9ec5456197cd655e99326c6
pepelev <kran440@gmail.com>
2020-07-16 00:34:12 +0500

    adds CustomResponseHandling command

---
+++ Rediska/Commands/Auxiliary/CommandExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Rediska.Commands.Auxiliary
+{
+    using Protocol.Visitors;
+    using Utils;
+
+    public static class CommandExtensions
+    {
+        public static Command<None> ResponseIgnoring<T>(this Command<T> command) => new ResponseIgnoring<T>(command);
+
+        public static Command<TResult> WithResponseStructure<TInner, TResult>(
+            this Command<TInner> command,
+            Visitor<TResult> responseStructure)
+            => new CustomResponseHandling<TInner, TResult>(command, responseStructure);
+    }
+}
---
+++ Rediska/Commands/Auxiliary/CustomResponseHandling.cs
@@ -0,0 +1,21 @@
+﻿namespace Rediska.Commands.Auxiliary
+{
+    using System;
+    using System.Collections.Generic;
+    using Protocol;
+    using Protocol.Visitors;
+
+    public sealed class CustomResponseHandling<TInner, TResult> : Command<TResult>
+    {
+        private readonly Command<TInner> command;
+
+        public CustomResponseHandling(Command<TInner> command, Visitor<TResult> responseStructure)
+        {
+            this.command = command ?? throw new ArgumentNullException(nameof(command));
+            ResponseStructure = responseStructure ?? throw new ArgumentNullException(nameof(responseStructure));
+        }
+
+        public override IEnumerable<BulkString> Request(BulkStringFactory factory) => command.Request(factory);
+        public override Visitor<TResult> ResponseStructure { get; }
+    }
+}
