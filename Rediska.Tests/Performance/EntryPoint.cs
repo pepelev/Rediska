447b6c45212d5f5a61c63a37ec58a9bd9661fd7c
pepelev <kran440@gmail.com>
2020-07-17 00:35:26 +0500

    tests should run on dotnet test

--- Rediska.Tests/Performance/EntryPoint.cs
+++ Rediska.Tests/Performance/EntryPoint.cs
@@ -4,7 +4,7 @@ namespace Rediska.Tests.Performance
 {
     public static class EntryPoint
     {
-        public static int Main()
+        public static int Main2()
         {
             var summary = BenchmarkRunner.Run<CallCost>();
             return 0;
