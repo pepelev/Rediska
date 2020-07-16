bef48440a9ea5fec883c64a1f9b470478f94e359
pepelev <kran440@gmail.com>
2020-07-16 23:43:08 +0500

    complete write Connection commands

---
+++ Rediska/Commands/Connection/AUTH.cs
@@ -0,0 +1,26 @@
+﻿namespace Rediska.Commands.Connection
+{
+    using System.Collections.Generic;
+    using Protocol;
+    using Protocol.Visitors;
+    using Utils;
+
+    public sealed class AUTH : Command<None>
+    {
+        private static readonly PlainBulkString name = new PlainBulkString("AUTH");
+        private readonly string password;
+
+        public AUTH(string password)
+        {
+            this.password = password;
+        }
+
+        public override IEnumerable<BulkString> Request(BulkStringFactory factory) => new[]
+        {
+            name,
+            factory.Utf8(password)
+        };
+
+        public override Visitor<None> ResponseStructure => OkExpectation.Singleton;
+    }
+}
