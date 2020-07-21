2d232375d84b72fc451a7c79cd423717a9ebaae5
pepelev <kran440@gmail.com>
2020-07-21 10:40:35 +0500

    adds basic XPENDING command

---
+++ Rediska/Commands/Streams/XPENDING.Summary.cs
@@ -0,0 +1,55 @@
+﻿namespace Rediska.Commands.Streams
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Protocol;
+    using Protocol.Visitors;
+    using Utils;
+
+    public sealed partial class XPENDING
+    {
+        public sealed class Summary
+        {
+            private readonly Array reply;
+
+            public Summary(Array reply)
+            {
+                this.reply = reply;
+            }
+
+            public long PendingEntries => reply[0].Accept(IntegerExpectation.Singleton);
+
+            public Id SmallestPendingEntryId => Id.Parse(
+                reply[1].Accept(BulkStringExpectation.Singleton).ToString()
+            );
+
+            public Id GreatestPendingEntryId => Id.Parse(
+                reply[2].Accept(BulkStringExpectation.Singleton).ToString()
+            );
+
+            public IReadOnlyList<(ConsumerName Consumer, long PendingMessages)> Consumers
+            {
+                get
+                {
+                    return new PrettyReadOnlyList<(ConsumerName Consumer, long PendingMessages)>(
+                        new ProjectingReadOnlyList<DataType, (ConsumerName Consumer, long PendingMessages)>(
+                            reply[3].Accept(ArrayExpectation.Singleton),
+                            pair =>
+                            {
+                                var content = pair.Accept(ArrayExpectation.Singleton);
+                                var consumer = new ConsumerName(
+                                    content[0].Accept(BulkStringExpectation.Singleton).ToString()
+                                );
+                                var pendingMessages = long.Parse(
+                                    content[1].Accept(BulkStringExpectation.Singleton).ToString(),
+                                    CultureInfo.InvariantCulture
+                                );
+                                return (consumer, pendingMessages);
+                            }
+                        )
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ Rediska/Commands/Streams/XPENDING.cs
@@ -0,0 +1,37 @@
+﻿namespace Rediska.Commands.Streams
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Protocol;
+    using Protocol.Visitors;
+    using Utils;
+    using Array = Protocol.Array;
+
+    public sealed partial class XPENDING : Command<XPENDING.Summary>
+    {
+        private static readonly PlainBulkString name = new PlainBulkString("XPENDING");
+
+        private static readonly Visitor<Summary> responseStructure = ArrayExpectation2.Singleton
+            .Then(reply => new Summary(reply));
+
+        private readonly Key key;
+        private readonly GroupName groupName;
+
+        public XPENDING(Key key, GroupName groupName)
+        {
+            this.key = key;
+            this.groupName = groupName;
+        }
+
+        public override IEnumerable<BulkString> Request(BulkStringFactory factory) => new[]
+        {
+            name,
+            key.ToBulkString(factory),
+            groupName.ToBulkString(factory)
+        };
+
+        public override Visitor<Summary> ResponseStructure => responseStructure;
+    }
+}
