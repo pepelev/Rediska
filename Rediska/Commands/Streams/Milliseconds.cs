6273a735ff9d4ef33b361adff2a8bc7a4bef142f
pepelev <kran440@gmail.com>
2020-07-23 23:47:19 +0500

    implements XCLAIM

---
+++ Rediska/Commands/Streams/Milliseconds.cs
@@ -0,0 +1,32 @@
+﻿namespace Rediska.Commands.Streams
+{
+    using System;
+    using System.Globalization;
+    using Protocol;
+
+    public readonly struct Milliseconds : IEquatable<Milliseconds>, IComparable<Milliseconds>
+    {
+        public Milliseconds(long value)
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(value), value, "Must be non-negative");
+
+            Value = value;
+        }
+
+        public long Value { get; }
+        public int CompareTo(Milliseconds other) => Value.CompareTo(other.Value);
+        public bool Equals(Milliseconds other) => Value == other.Value;
+        public static bool operator ==(Milliseconds left, Milliseconds right) => left.Equals(right);
+        public static bool operator >(Milliseconds left, Milliseconds right) => left.CompareTo(right) > 0;
+        public static bool operator >=(Milliseconds left, Milliseconds right) => left.CompareTo(right) >= 0;
+        public static bool operator !=(Milliseconds left, Milliseconds right) => !left.Equals(right);
+        public static bool operator <(Milliseconds left, Milliseconds right) => left.CompareTo(right) < 0;
+        public static bool operator <=(Milliseconds left, Milliseconds right) => left.CompareTo(right) <= 0;
+        public BulkString ToBulkString(BulkStringFactory factory) => factory.Create(Value);
+        public TimeSpan ToTimeSpan() => new TimeSpan(TimeSpan.TicksPerMillisecond * Value);
+        public override bool Equals(object obj) => obj is Milliseconds other && Equals(other);
+        public override int GetHashCode() => Value.GetHashCode();
+        public override string ToString() => ToTimeSpan().ToString("g", CultureInfo.InvariantCulture);
+    }
+}
---
+++ Rediska/Commands/Streams/XCLAIM.cs
@@ -0,0 +1,50 @@
+﻿namespace Rediska.Commands.Streams
+{
+    using System;
+    using System.Collections.Generic;
+    using Protocol;
+    using Protocol.Visitors;
+
+    public sealed class XCLAIM : Command<IReadOnlyList<Entry>>
+    {
+        private static readonly PlainBulkString name = new PlainBulkString("XCLAIM");
+        private readonly Key key;
+        private readonly GroupName groupName;
+        private readonly ConsumerName consumerName;
+        private readonly Milliseconds minimumIdleTime;
+        private readonly IReadOnlyList<Id> ids;
+
+        public XCLAIM(
+            Key key,
+            GroupName groupName,
+            ConsumerName consumerName,
+            Milliseconds minimumIdleTime,
+            IReadOnlyList<Id> ids)
+        {
+            if (ids == null) throw new ArgumentNullException(nameof(ids));
+            if (ids.Count < 1)
+                throw new ArgumentException("Must contain items", nameof(ids));
+
+            this.key = key ?? throw new ArgumentNullException(nameof(key));
+            this.groupName = groupName;
+            this.consumerName = consumerName;
+            this.minimumIdleTime = minimumIdleTime;
+            this.ids = ids;
+        }
+
+        public override IEnumerable<BulkString> Request(BulkStringFactory factory)
+        {
+            yield return name;
+            yield return key.ToBulkString(factory);
+            yield return groupName.ToBulkString(factory);
+            yield return consumerName.ToBulkString(factory);
+            yield return minimumIdleTime.ToBulkString(factory);
+            foreach (var id in ids)
+            {
+                yield return id.ToBulkString(factory, Id.Print.Full);
+            }
+        }
+
+        public override Visitor<IReadOnlyList<Entry>> ResponseStructure => RangeConstants.ResponseStructure;
+    }
+}
